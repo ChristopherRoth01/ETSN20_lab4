b6dcd46330ccb935a0cc8d7b8932ea717b41aeb4
ChristopherRoth01 <47116031+ChristopherRoth01@users.noreply.github.com>
2023-11-30 09:34:19 +0100

    added program

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import cli.Session;
+import logic.FileSearcher;
+
+
+public class Main {
+
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        Session session = new Session();
+        session.run();
+    }
+}
